6225212d89bc927845f1d7b43d3084eb80723ce5
Krzysztof Durbajło <krzdur@gmail.com>
2022-03-06 17:22:43 +0100

    update example of experiment analysis

--- examples/queries/experiment_query.sql
+++ examples/queries/experiment_query.sql
@@ -1,18 +1,35 @@
+WITH user_stats AS (
+    SELECT fullVisitorId,
+           SUM(
+                   CASE WHEN hits.eventInfo.eventAction = 'Product Click' THEN 1 END
+               ) prod_clicks,
+           SUM(
+                   CASE WHEN hits.eventInfo.eventAction = 'Add to Cart' THEN 1 END
+               ) add_to_cart_clicks,
+    FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
+         UNNEST(hits) AS hits
+
+    WHERE 1 = 1
+      AND _TABLE_SUFFIX BETWEEN '20170101' AND '20170801'
+      AND hits.eventInfo.eventAction IN ('Product Click', 'Add to Cart')
+
+    GROUP BY 1
+)
+
 SELECT CAST(
                MOD(CAST(fullVisitorId AS NUMERIC), 2)
-               AS INT) as                 variant, # as there were no experiments run on the sample data, we're mocking it
+           AS INT) AS                variant, # as there were no experiments run on the sample data, we're mocking it
        COUNT(DISTINCT fullVisitorId) users,
-       COUNT(DISTINCTa
-             CASE WHEN hits.eventInfo.eventAction = 'Product Click' THEN fullVisitorId END
+       COUNT(DISTINCT
+             CASE WHEN prod_clicks > 0 THEN fullVisitorId END
            )                         users_prod_click,
        COUNT(DISTINCT
-             CASE WHEN hits.eventInfo.eventAction = 'Add to Cart' THEN fullVisitorId END
+             CASE WHEN add_to_cart_clicks > 0 THEN fullVisitorId END
            )                         users_add_to_cart,
+       AVG(prod_clicks)              avg_prod_clicks,
+       AVG(add_to_cart_clicks)       avg_add_to_cart_clicks,
+       VARIANCE(prod_clicks)         var_prod_clicks,
+       VARIANCE(add_to_cart_clicks)  var_add_to_cart_clicks,
 
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
-     UNNEST(hits) AS hits
-
-WHERE 1 = 1
-  AND _TABLE_SUFFIX BETWEEN '20170101' AND '20170801'
-
+FROM user_stats
 GROUP BY 1
